8be14fce38bc0b762ec64620605ed8ff6bf19f19
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-08-07 10:28:06 +0200

    add pages and routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/page2', function () {
+    return view('page2');
+});
+
+Route::get('/page3', function () {
+    return view('page3');
 });
